10b5d666016a92be359e7a30453ab421d1381541
Seth <sethrhowell@gmail.com>
2018-11-13 10:12:26 -0500

    Merged "mobile" version and desktop. Also added getting responses and continuing through scenes.
    Signed-off-by: Seth <sethrhowell@gmail.com>

--- Messing Around/Program.cs
+++ Messing Around/Program.cs
@@ -1,197 +1,208 @@
 ﻿using System;
-using System.ComponentModel.Design;
-using System.Diagnostics;
-using System.Linq;
+using System.Net;
+using System.Net.Mime;
 using System.Threading;
 
-namespace Messing_Around
+namespace MessingAroundMobile
 {
     internal class Program
     {
-        static string getName()
+        static void displaySetting(string settingText) // Working on making this print out words character-by-character
         {
-            // Ask for name, get name and return it. Is there a point in modularizing this? maybe not.
-            Console.WriteLine("Please enter your name...");
+            char[] setting = settingText.ToCharArray();
+            
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            
+            for (int i = 0; i < setting.Length; i++)
+            {
+                switch (setting[i])
+                {
+                    case '\\':
+                        // Yellow
+                        if (setting[i + 1] == 'Y')
+                        {
+                            setting[i] = '\0';
+                            setting[i + 1] = '\0';
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                        }
+
+                        // Dark Yellow
+                        if (setting[i + 1] == 'D' && setting[i + 2] == 'Y')
+                        {
+                            setting[i] = '\0';
+                            setting[i + 1] = '\0';
+                            setting[i + 2] = '\0';
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        }
+                        
+                        // White
+                        if (setting[i + 1] == 'W')
+                        {
+                            setting[i] = '\0';
+                            setting[i + 1] = '\0';
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        
+                        // Longer pause, seconds based on number after \L (example: \L1 will extend the wait by 1 second)
+                        if (setting[i + 1] == 'L' && int.Parse(setting[i + 2].ToString()) > 0)
+                        {
+                            int customSleep = int.Parse(setting[i + 2].ToString());
+                            setting[i] = '\0';
+                            setting[i + 1] = '\0';
+                            setting[i + 2] = '\0';
+                            Thread.Sleep(customSleep * 100);
+                        }
+
+                        break;
+                    case '.':
+                        Console.Write(setting[i]);
+                        Thread.Sleep(150);
+
+                        break;
+                    case ',':
+                        Console.Write(setting[i]);
+                        Thread.Sleep(150);
+
+                        break;
+                    default:
+                        if (setting[i] != '\0')
+                        {
+                            Console.Write(setting[i]);
+                            Thread.Sleep(35);
+                        }
+                        
+                        break;
+                }
+            }
+        }
+
+        static string[] displayActions(string[] actions) // Work on this later
+        {
+            // Creating actions method, should allow for more... user-friendly input?
+            Console.WriteLine();
+            
             Console.ForegroundColor = ConsoleColor.Yellow;
-            string name = Console.ReadLine();
+            
+            for (int i = 0; i < actions.Length; i++)
+            {
+                Console.Write("[{0}]     ", actions[i].ToUpper());
+            }
+            
+            Console.Write("[QUIT]");
+
             Console.ForegroundColor = ConsoleColor.White;
 
-            return name;
+            return actions;
         }
 
-        static void setting(string text, string[] commands)
+        static string getAction(string[] actions) // Focus on after text manipulation
         {
-            // Use a generic setting with customized menu.
-            Console.WriteLine(text);
-            actions(commands);
-        }
-
-        static void actions(string[] commands)
-        {
-            for (int i = 0; i < commands.Length; i++)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write("[{0}]   ", commands[i].ToUpper());
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            
-            // Start a new line for the response.
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine();
-        }
+            string action = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
 
-        static string getAction(string[] commands)
-        {
-            string text = Console.ReadLine();
-            
-            while (text != "")
+            while (action != "")
             {
-                for (int i = 0; i < commands.Length; i++)
+                for (int i = 0; i < actions.Length; i++)
                 {
-                    if (text.ToLower() == commands[i])
+                    if (action.ToLower() == actions[i])
                     {
-                        return commands[i];
+                        return actions[i];
                     }
                 }
-                Console.WriteLine("Sorry, that is not a valid option.");
-                actions(commands);
-                text = Console.ReadLine();
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("That is not a valid action.");
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                action = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.White;
             }
 
-            return "Something went wrong!";
+            return "Error";
         }
 
-        static bool respond(string command, string[] commands, string[] responses, bool[] cont)
+        static bool getResponse(string action, string[] actions, string[] responses, bool[] next)
         {
-            // Checking entered command against the given commands and giving respective response.
-            for (int i = 0; i < commands.Length; i++)
+            for (int i = 0; i < next.Length; i++)
             {
-                if (command == commands[i])
+                if (action.ToLower() == actions[i])
                 {
-                    if (command == "quit")
+                    if (next[i] == true)
                     {
-                        Environment.Exit(1);
-                    }
-
-                    if (!cont[i])
-                    {
-                        Console.WriteLine(responses[i]);
-                        actions(commands);
-                        
-                        return false;
-                    }
-                    else // If it's true
-                    {
-                        Console.WriteLine(responses[i]);
-                        
                         return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine(responses[i]);
+                        return false;
                     }
                 }
             }
 
             return false;
         }
-
-        static void replaceAndSleep(int seconds)
+        
+        static void replaceAndWait(int seconds)
         {
-            // Move console write cursor to the beginning of the string to overwrite it.
+            // Adjusts console cursor to the beginning of the last message to replace it, and then waits for the given time.
             Console.CursorTop -= 1;
             Console.CursorLeft = 0;
-            
-            // Convert to milliseconds and sleep.
-            Thread.Sleep(seconds*100);
+            Thread.Sleep(seconds * 100);
         }
-
-        static void clearAndSleep(int seconds)
-        {
-            // Convert to milliseconds and sleep.
-            Thread.Sleep(seconds*100);
-            
-            // Clear the entire console.
-            Console.Clear();
-        }
-
+        
         public static void Main(string[] args)
         {
-            Console.WriteLine("Version 0.001");
-
-            string[] commands;
-            string[] responses;
-            bool[] cont;
-            bool next = false;
+            string name = "Dummy";
+            string[] actions = { };
+            string[] responses = { };
+            bool cont = false;
+            bool[] next = { };
             
-            Name:
-            string name = getName();
-
-            Thread.Sleep(100);
-            Console.WriteLine("Your name is {0}? [enter Y or N to proceed]", name);
+            // Ask for name, use system name as default.
+            Console.WriteLine("Is your name {0}?", Environment.UserName);
+            Console.WriteLine("ENTER \"Y\" OR \"N\" TO ANSWER]");
             Console.ForegroundColor = ConsoleColor.Yellow;
-            string choice = Console.ReadLine();
+            string answer = Console.ReadLine();
             Console.ForegroundColor = ConsoleColor.White;
 
-            if (choice == "Y")
+            // If the system name is not preferred, then get the real name.
+            if (answer.ToLower() == "n")
             {
-                Console.WriteLine("Initializing game.exe");
-                replaceAndSleep(5);
-                Console.WriteLine("Initializing game.exe.");
-                replaceAndSleep(5);
-                Console.WriteLine("Initializing game.exe..");
-                replaceAndSleep(5);
-                Console.WriteLine("Initializing game.exe...");
-            }
-            else
-            {
-                goto Name;
+                Console.WriteLine("Please enter your preferred name:");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                name = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Okay, {0}. We will start the program.", name);
             }
 
-            clearAndSleep(3);
+            // Lets the user know the program is starting.
+            Console.WriteLine("Starting");
+            replaceAndWait(3);
+            Console.WriteLine("Starting.");
+            replaceAndWait(3);
+            Console.WriteLine("Starting..");
+            replaceAndWait(3);
+            Console.WriteLine("Starting...");
+            Thread.Sleep(2 * 100);
+            Console.Clear();
+            
+            tutorial:
+            actions = new string[] {"continue"};
+            next = new bool[] {true};
+            responses = new string[] {"Error"};
+            
+            displaySetting("\\DYWelcome to Seth's Dating Sim, where you can live out your desires of dating.\\L1.\\L1.\\L2 \\YBartosz Burda\\DY!!!!!!!!!!!!");
 
-            Console.WriteLine(".");
-            replaceAndSleep(5);
-            Console.WriteLine("..");
-            replaceAndSleep(5);
-            Console.WriteLine("...");
-
-            clearAndSleep(3);
+            while (cont == false)
+            {
+                cont = getResponse(getAction(displayActions(actions)), actions, responses, next);
+            }
             
             sceneOne:
-            commands = new string[] {"look", "feel", "quit"};
-            responses = new string[]
-                {"You can't see anything.", "You feel a smooth surface below your feet."};
-            cont = new bool[] {false, true, false};
-            setting("You appear to be in a dark room, with nothing but your sense of touch to guide you.", commands);
-
-            while (!next)
-            {
-                next = respond(getAction(commands), commands, responses, cont);
-            }
-            
-            sceneTwo:
-            commands = new string[] {"look", "feel more", "quit"};
-            responses = new string[]
-                {"Surprisingly, you still can't see anything.", "As your reach to feel the ground again, a light above comes to life."};
-            cont = new bool[] {false, true, false};
-            setting("The room is still dark, but your surroundings almost feel... easier to see.", commands);
-
-            next = false;
-            
-            while (!next)
-            {
-                next = respond(getAction(commands), commands, responses, cont);
-            }
-            
-            sceneThree:
-            commands = new string[] {"look", "talk", "quit"};
-            responses = new string[]
-                {"The room is plain white, with a single dark brown wooden door in the center. A burly man stands in front, staring you down.", "You try speaking, but you seem to be muted."};
-            cont = new bool[] {false, true, false};
-            setting("A light now hands from the ceiling of the short white room, illuminating a single door and a man at its center. He looks intimidating.", commands);
-
-            next = false;
-            
-            while (!next)
-            {
-                next = respond(getAction(commands), commands, responses, cont);
-            }
+            displaySetting("\\YBart \\DYappears in front of you. What do you do?");
+            Console.WriteLine(getAction(displayActions(new string[] {"ATTACK", "FLIRT"})));
         }
     }
 }
