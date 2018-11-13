7ba60cf3ead28de00b9e70913648d7d257187ba0
Seth <sethrhowell@gmail.com>
2018-11-13 12:50:40 -0500

    Added richText module for formatted text instead of having it inside the displaySetting. Changed displaySetting to initializeSetting so that we can just have one line instead of a bunch when having a new scenario and such.
    Signed-off-by: Seth <sethrhowell@gmail.com>

--- Messing Around/Program.cs
+++ Messing Around/Program.cs
@@ -7,79 +7,121 @@ namespace MessingAroundMobile
 {
     internal class Program
     {
-        static void displaySetting(string settingText) // Working on making this print out words character-by-character
+        static void setColor(string color)
         {
-            char[] setting = settingText.ToCharArray();
-            
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            
-            for (int i = 0; i < setting.Length; i++)
+            switch (color.ToLower())
             {
-                switch (setting[i])
+                case "white":
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case "yellow":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case "blue":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+        }
+
+        static void richText(string input) // Making rich text module
+        {
+            char[] text = input.ToCharArray();
+            
+            for (int i = 0; i < text.Length; i++)
+            {
+                switch (text[i])
                 {
                     case '\\':
                         // Yellow
-                        if (setting[i + 1] == 'Y')
+                        if (text[i + 1] == 'Y')
                         {
-                            setting[i] = '\0';
-                            setting[i + 1] = '\0';
+                            text[i] = '\0';
+                            text[i + 1] = '\0';
                             Console.ForegroundColor = ConsoleColor.Yellow;
                         }
                         
-                        if (setting[i + 1] == 'B')
+                        // Blue
+                        if (text[i + 1] == 'B')
                         {
-                            setting[i] = '\0';
-                            setting[i + 1] = '\0';
+                            text[i] = '\0';
+                            text[i + 1] = '\0';
                             Console.ForegroundColor = ConsoleColor.Blue;
+                        }
+                        
+                        // Green
+                        if (text[i + 1] == 'G')
+                        {
+                            text[i] = '\0';
+                            text[i + 1] = '\0';
+                            Console.ForegroundColor = ConsoleColor.Green;
                         }
 
                         // Dark Yellow
-                        if (setting[i + 1] == 'D' && setting[i + 2] == 'Y')
+                        if (text[i + 1] == 'D' && text[i + 2] == 'Y')
                         {
-                            setting[i] = '\0';
-                            setting[i + 1] = '\0';
-                            setting[i + 2] = '\0';
+                            text[i] = '\0';
+                            text[i + 1] = '\0';
+                            text[i + 2] = '\0';
                             Console.ForegroundColor = ConsoleColor.DarkYellow;
                         }
                         
                         // White
-                        if (setting[i + 1] == 'W')
+                        if (text[i + 1] == 'W')
                         {
-                            setting[i] = '\0';
-                            setting[i + 1] = '\0';
+                            text[i] = '\0';
+                            text[i + 1] = '\0';
                             Console.ForegroundColor = ConsoleColor.White;
                         }
                         
                         // Longer pause, seconds based on number after \L (example: \L1 will extend the wait by 1 second)
-                        if (setting[i + 1] == 'L' && int.Parse(setting[i + 2].ToString()) > 0)
+                        if (text[i + 1] == 'L' && int.Parse(text[i + 2].ToString()) > 0)
                         {
-                            int customSleep = int.Parse(setting[i + 2].ToString());
-                            setting[i] = '\0';
-                            setting[i + 1] = '\0';
-                            setting[i + 2] = '\0';
+                            int customSleep = int.Parse(text[i + 2].ToString());
+                            text[i] = '\0';
+                            text[i + 1] = '\0';
+                            text[i + 2] = '\0';
                             Thread.Sleep(customSleep * 100);
                         }
 
                         break;
                     case '.':
-                        Console.Write(setting[i]);
+                        Console.Write(text[i]);
                         Thread.Sleep(150);
 
                         break;
                     case ',':
-                        Console.Write(setting[i]);
+                        Console.Write(text[i]);
                         Thread.Sleep(150);
 
                         break;
                     default:
-                        if (setting[i] != '\0')
+                        if (text[i] != '\0')
                         {
-                            Console.Write(setting[i]);
+                            Console.Write(text[i]);
                             Thread.Sleep(35);
                         }
                         
                         break;
                 }
+            }
+        }
+        
+        static void initializeScene(string settingText, string[] actions, string[] responses, bool[] next)
+        {
+            bool cont = false;
+            
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            richText(settingText);
+
+            cont = canContinue(actions, responses, next);
+            
+            while (!cont)
+            {
+                cont = canContinue(actions, responses, next);
             }
         }
 
@@ -113,6 +155,11 @@ namespace MessingAroundMobile
             {
                 for (int i = 0; i < actions.Length; i++)
                 {
+                    if (action.ToLower() == "quit")
+                    {
+                        Environment.Exit(1);
+                    }
+                    
                     if (action.ToLower() == actions[i])
                     {
                         return actions[i];
@@ -138,22 +185,38 @@ namespace MessingAroundMobile
                 {
                     if (next[i] == true)
                     {
-                        Console.ForegroundColor = ConsoleColor.Blue;
+                        setColor("blue");
                         Console.WriteLine("[{0}]", actions[i].ToUpper());
-                        Console.ForegroundColor = ConsoleColor.White;
-                        displaySetting(responses[i]);
+                        setColor("white");
+                        Console.WriteLine();
+                        richText(responses[i]);
                         Console.WriteLine();
                         return true;
                     }
                     else
                     {
-                        Console.WriteLine(responses[i]);
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                        Console.WriteLine("[{0}]", actions[i].ToUpper());
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine();
+                        richText(responses[i]);
                         return false;
                     }
                 }
             }
 
             return false;
+        }
+
+        static bool canContinue(string[] actions, string[] responses, bool[] next)
+        {
+            bool cont = false;
+            
+            while (cont == false)
+            {
+                cont = getResponse(getAction(displayActions(actions)), actions, responses, next);
+            }
+            return cont;
         }
         
         static void replaceAndWait(int seconds)
@@ -163,15 +226,16 @@ namespace MessingAroundMobile
             Console.CursorLeft = 0;
             Thread.Sleep(seconds * 100);
         }
-        
+
         public static void Main(string[] args)
         {
             string name = "Dummy";
+            string settingText = "Empty";
             string[] actions = { };
             string[] responses = { };
             bool cont = false;
             bool[] next = { };
-            
+
             // Ask for name, use system name as default.
             Console.WriteLine("Is your name {0}?", Environment.UserName);
             Console.WriteLine("ENTER \"Y\" OR \"N\" TO ANSWER]");
@@ -199,32 +263,50 @@ namespace MessingAroundMobile
             Console.WriteLine("Starting...");
             Thread.Sleep(2 * 100);
             Console.Clear();
-            
+
             tutorial:
-            actions = new string[] {"continue"};
+            settingText =
+                "\\DYWelcome to Seth's Dating Sim, where you can live out your desires of dating.\\L1.\\L1.\\L2 \\YBartosz Burda\\DY!!!!!!!!!!!!";
+            actions = new string[] {"play"};
             next = new bool[] {true};
-            responses = new string[] {"\\DYIn this game, you will be presented with a \\Yscenario \\DYwith its own \\Yactions\\DY."};
-            
-            displaySetting("\\DYWelcome to Seth's Dating Sim, where you can live out your desires of dating.\\L1.\\L1.\\L2 \\YBartosz Burda\\DY!!!!!!!!!!!!");
+            responses = new string[]
+                {"\\DYIn this game, you will be presented with a \\Yscenario \\DYwith its own \\Yactions\\DY."};
+            initializeScene(settingText, actions, responses, next);
 
-            while (cont == false)
-            {
-                cont = getResponse(getAction(displayActions(actions)), actions, responses, next);
-            }
+            settingText =
+                "\\DYTo choose an \\Yaction\\DY, simply type exactly what's in the brackets. (ex. \\Y[CONTINUE] \\DY would be entered as \"\\Ycontinue\\DY\")";
+            actions = new string[] {"continue"};
+            responses = new string[] {"\\DYGood! Some \\Yactions \\DYwill not lead to a new \\Yscenario\\DY."};
+            initializeScene(settingText, actions, responses, next);
 
-            cont = false;
-            responses = new string[] {"temp"};
-            
-            displaySetting("\\DYSimply type exactly what's in the brackets. (ex. \\Y[CONTINUE] \\DY would be entered as \"\\Ycontinue\\DY.\"");
-            
-            while (cont == false)
-            {
-                cont = getResponse(getAction(displayActions(actions)), actions, responses, next);
-            }
-            
+            settingText = "\\DYChoose \\Y[USELESS] \\DYto see an example of a limited action.";
+            actions = new string[] {"useless", "continue"};
+            responses = new string[]
+                {"\\DYGreat! Now you can choose \\Y[CONTINUE] \\DYto move on to the real game.\nHave fun!", ""};
+            next = new bool[] {false, true};
+            initializeScene(settingText, actions, responses, next);
+
             sceneOne:
-            displaySetting("\\YBart \\DYappears in front of you. What do you do?");
-            Console.WriteLine(getAction(displayActions(new string[] {"ATTACK", "FLIRT"})));
+            settingText = "\\DYA wild \\GBartosz Burda \\DYappears in front of you!\nWhat should you do?";
+            actions = new string[] {"attack", "flirt", "run"};
+            next = new bool[] {false, true, false};
+            responses = new string[]
+            {
+                "\\DYYou throw a punch, but he catches it right before it hits him.\n\\YBart: \\W'Sup, you got a snap?\n\\DYYou mumble your Snapchat username, but he doesn't understand.",
+                "\\DYYou wink at him awkwardly, your mouth twitching as you try to hold it.", "\\DYYou can't run from fate."
+            };
+            initializeScene(settingText, actions, responses, next);
+
+            settingText =
+                "\\DYBart pulls a \\Ytoothpick \\DYout of a pocket on his jacket sleeve, sliding it between his teeth.\n\\YBart: \\WWeirdo. Hold on, gotta get this egg out of my teeth.";
+            actions = new string[] {"flirt", "toothpick?"};
+            next = new bool[] {false, true};
+            responses = new string[]
+            {
+                "\\DYYou try winking again, but he continues picking his teeth unimpressed",
+                "\\DYYou comment on his \\Ytoothpick\\DY, noting that he looks 10% cooler with it in."
+            };
+            initializeScene(settingText, actions, responses, next);
         }
     }
 }
