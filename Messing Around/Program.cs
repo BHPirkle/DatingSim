d858c5923679851edd3c6579aa7291c1026dbfe3
Seth <sethrhowell@gmail.com>
2018-11-16 00:50:49 -0500

    Finalize the game, hoping to build it and put the release on the repo... we'll see.
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- Messing Around/Program.cs
+++ Messing Around/Program.cs
@@ -226,7 +226,7 @@ namespace MessingAroundMobile
                     }
                     else
                     {
-                        setColor("db");
+                        setColor("w");
                         Console.WriteLine("[{0}]", actions[i].ToUpper());
                         setColor("w");
                         Console.WriteLine();
@@ -268,7 +268,7 @@ namespace MessingAroundMobile
 
             // Ask for name, use system name as default.
             Console.WriteLine("Is your name {0}?", Environment.UserName);
-            
+
             Console.WriteLine("[ENTER \"Y\" OR \"N\" TO ANSWER]");
             setColor("y");
             string answer = Console.ReadLine();
@@ -295,11 +295,21 @@ namespace MessingAroundMobile
             Thread.Sleep(2 * 100);
             Console.Clear();
 
-            if (args[0] == "-skip")
+            switch (args[0])
             {
-                goto sceneTwo;
+                case "-scene1":
+                    goto sceneOne;
+                    break;
+                case "-scene2":
+                    goto sceneTwo;
+                    break;
+                case "-scene3":
+                    goto sceneThree;
+                    break;
+                default:
+                    break;
             }
-            
+
             tutorial:
             // Pretty self-explanatory.
             settingText =
@@ -311,7 +321,7 @@ namespace MessingAroundMobile
             initializeScene(settingText, actions, responses, next);
 
             settingText =
-                "\\DYTo choose an \\Yaction\\DY, simply type exactly what's in the brackets. (ex. \\Y[CONTINUE] \\DY would be entered as \"\\Ycontinue\\DY\")";
+                "\\DYTo choose an \\Yaction\\DY, simply type exactly what's in the brackets. (ex. \\Y[CONTINUE] \\DYwould be entered as \"\\Ycontinue\\DY\")";
             actions = new string[] {"continue"};
             responses = new string[] {"\\DYGood! Some \\Yactions \\DYwill not lead to a new \\Yscenario\\DY."};
             initializeScene(settingText, actions, responses, next);
@@ -319,7 +329,7 @@ namespace MessingAroundMobile
             settingText = "\\DYChoose \\Y[USELESS] \\DYto see an example of a limited action.";
             actions = new string[] {"useless", "continue"};
             responses = new string[]
-                {"\\DYGreat! Now you can choose \\Y[CONTINUE] \\DYto move on to the real game.\nHave fun!", ""};
+                {"\\DYGreat! Now you can choose \\Y[CONTINUE] \\DYto move on to the real game.\nHave fun!", "\\YStarting the real game\\L1.\\L1.\\L1.\\L1"};
             next = new bool[] {false, true};
             initializeScene(settingText, actions, responses, next);
 
@@ -331,7 +341,8 @@ namespace MessingAroundMobile
             responses = new string[]
             {
                 "\\DYYou throw a punch, but he catches it right before it hits him.\n\\YBart: \\W'Sup, you got a snap?\n\\DYYou mumble your Snapchat username, but he doesn't understand.",
-                "\\DYYou wink at him awkwardly, your mouth twitching as you try to hold it.", "\\DYYou can't run from fate."
+                "\\DYYou wink at him awkwardly, your mouth twitching as you try to hold your eye shut.",
+                "\\DYYou can't run from fate."
             };
             initializeScene(settingText, actions, responses, next);
 
@@ -351,7 +362,11 @@ namespace MessingAroundMobile
             actions = new string[] {"flirt", "stupid", "snap"};
             next = new bool[] {false, true, false};
             responses = new string[]
-                {"\\DYYou fiddle with your hair, unsure of what other moves you could make besides winking.", "\\DYYou tell him the \\Ytoothpick \\DYmakes him look silly. He bites down and breaks it, the \\Ytoothpick \\DYfalling to the floor.", "\\DYYou open Snapchat on your phone, offering your \\YQR code\\DY. He doesn't move."};
+            {
+                "\\DYYou fiddle with your hair, unsure of what other moves you could make besides winking.",
+                "\\DYYou tell him the \\Ytoothpick \\DYmakes him look silly. He bites down and breaks it, the \\Ytoothpick \\DYfalling to the floor.",
+                "\\DYYou open Snapchat on your phone, offering your \\YQR code\\DY. He doesn't move."
+            };
             initializeScene(settingText, actions, responses, next);
 
             sceneTwo:
@@ -359,27 +374,32 @@ namespace MessingAroundMobile
             settingText = "\\R\\L1H\\L1e\\L1'\\L1s\\L1 \\L1m\\L1a\\L1d\\L1.";
             actions = new string[] {"attack", "flirt", "run"};
             next = new bool[] {true, false, false};
-            responses = new string[] {"\\DRYou throw a punch and hit \\RBart \\DRdirectly in the chest.\n\\DR[-5 HP]", "\\DYYou try \\Ydouble-winking \\DYto impress him, but he's teeming with anger.", "\\DYYou think about running, but can't since there's no controls for that."};
+            responses = new string[]
+            {
+                "\\DRYou throw a punch and hit \\RBart \\DRdirectly in the chest.\n\\DR[-5 HP]",
+                "\\DYYou try \\Ydouble-winking \\DYto impress him, but he's teeming with anger.",
+                "\\DYYou think about running, but can't since there's no controls for that."
+            };
             initializeScene(settingText, actions, responses, next);
-            
-            display("\\RHP [\\G||||||||||\\R]");
+
+            display("\\RBart [\\G■■■■■■■■■■\\R]");
             Console.WriteLine();
             replaceAndWait(2);
-            display("\\RHP [\\G|||||||||\\R|]");
+            display("\\RBart [\\G■■■■■■■■■\\R■]");
             Console.WriteLine();
-            
+
             settingText = "\\RBart: \\WYou're gonna regret that! I'm a freaking \\R\\L1p\\L1i\\L1l\\L1o\\L1t\\W bro!!!";
             actions = new string[] {"attack", "apologize", "flirt", "run"};
             next = new bool[] {false, true, false, false};
             responses = new string[]
             {
                 "\\DYYou throw another punch, but he simply moves to the side.",
-                "\\DYYou apologize for hurting him in hopes of mercy. \\YBart \\DYblushes and looks down.\n\\RBart: \\WI'm really just a softy, I don't actually like fighting\\L1.\\L1.\\L1.\\L1 I'm sorry\\L1.\\L1.\\L1.",
+                "\\DYYou apologize for hurting him in hopes of mercy. \\YBart \\DYblushes and looks down.\n\\YBart: \\WI'm really just a softy, I don't actually like fighting\\L1.\\L1.\\L1.\\L1 I'm sorry\\L1.\\L1.\\L1.",
                 "\\DYYou ask him for his snap, but he's still fuming.",
                 "\\DYYou wish you were able to use \\YWASD\\DY, but this is only a text game."
             };
             initializeScene(settingText, actions, responses, next);
-            
+
             settingText =
                 "\\RBart \\DRpunches you in the gut.\\L7 You clutch yourself in pain.\n\\RBart: \\WHa! Just kidding!";
             actions = new string[] {"attack", "joke", "flirt", "run"};
@@ -392,11 +412,11 @@ namespace MessingAroundMobile
                 "\\DRLOOK! I \\L2C\\L2A\\L2N\\L2'\\L2T just add movement to a text game! Stop choosing this action!"
             };
             initializeScene(settingText, actions, responses, next);
-            
-            display("\\RHP [\\G|||||||||\\R|]");
+
+            display("\\RBart [\\G■■■■■■■■■\\R■]");
             Console.WriteLine();
             replaceAndWait(2);
-            display("\\RHP [\\G|||||||\\R|||]");
+            display("\\RBart [\\G■■■■■■■\\R■■■]");
             Console.WriteLine();
 
             settingText = "\\RBart: \\WI'm gonna break you, just like.\\L1.\\L1.\\L3 \\RCharlotte\\W!!!";
@@ -409,12 +429,76 @@ namespace MessingAroundMobile
                 "\\DRYou can't run."
             };
             initializeScene(settingText, actions, responses, next);
-            
-            display("\\RHP [\\G|||||||\\R|||]");
+
+            display("\\RBart [\\G■■■■■■■\\R■■■]");
             Console.WriteLine();
             replaceAndWait(2);
-            display("\\RHP [\\G|||||\\R|||||]");
+            display("\\RBart [\\G■■■■■\\R■■■■■]");
             Console.WriteLine();
+
+            sceneThree:
+            settingText =
+                "\\RBart: \\WAlright, that \\L1a\\L1c\\L1t\\L1u\\L1a\\L1l\\L1l\\L1y kinda hurt...\n\\DRBart ends the fight!";
+            actions = new string[] {"weak", "ask out"};
+            next = new bool[] {false, true};
+            responses = new string[]
+            {
+                "\\DYYou berate \\YBart \\DYfor being weak.\\L2\nHe's not amused.",
+                "\\DYImpressed by his \\Ystrength\\DY, you tenderly ask him to be your boyfriend.\n\\YBart: \\WOh, uh\\L1.\\L1.\\L1.\\L1 I guess so!!! Why not???"
+            };
+            initializeScene(settingText, actions, responses, next);
+
+            settingText = "\\DYThe room is filled with a visible \\Yawkwardness\\DY.";
+            actions = new string[] {"small talk", "interests", "free agent?"};
+            next = new bool[] {true, false, false};
+            responses = new string[]
+            {
+                "\\DYYou try to come up with something to say, but all you can think of is the visible \\Yegg \\DYstill in his teeth from earlier.",
+                "\\DYYou try asking him what he's interested in, but he's busy staring at his phone.\n\\YBart: \\WOh, sorry. I was looking at Csikos memes. What'd you say?\n\\DYYou don't feel like repeating yourself.",
+                "\\DYYou ask him what being a \"\\Yfree agent\\DY\" means.\n\\YBart: \\WUh... \\L2nothing! It just means I like to do fun things... \\L2 yeah!"
+            };
+            initializeScene(settingText, actions, responses, next);
+
+            settingText =
+                "\\YBart \\DYchuckles nervously, crossing is arms before he speaks.\n\\YBart: \\WHey, so uh\\L1.\\L1.\\L1.\\L1 I think this relationship has run it's course. I want to be a \\Yfree agent\\W again, but it was nice being with you.\n\\DYYou've only been together for a couple minutes.";
+            actions = new string[] {"nod"};
+            next = new bool[] {true};
+            responses = new string[] {"\\DYYou just nod your head, astonished by the suggestion."};
+            initializeScene(settingText, actions, responses, next);
+
+            settingText = "\\YBart: \\W\\L3.\\L3.\\L3.\\L3Wanna stay friends with benefits???";
+            actions = new string[] {"no"};
+            next = new bool[] {true};
+            responses = new string[] {"\\DYYou shake your head, still in disbelief."};
+            initializeScene(settingText, actions, responses, next);
+
+            settingText =
+                "\\YBart: \\WHaha, I was just kidding anyway... yeah. I'm gonna go \"hang\" with \\YCharlotte, \\Wsee ya later loser.";
+            actions = new string[] {"end"};
+            next = new bool[] {true};
+            responses = new string[]
+            {
+                "\\YBart \\DYwalks away, leaving you alone. Stranded. Thirsting for more \\YBart\\DY.\n\n\\L3\\B\\L1T\\L1H\\L1E \\L1E\\L1N\\L1D"
+            };
+            initializeScene(settingText, actions, responses, next);
+
+            settingText =
+                "\\DYThanks for playing the game. It was a silly joke, but helped me learn a bit of C#. I gotta get back to the books though, since I'm only just beginning.\nIf you want more info, don't choose quit.";
+            actions = new string[] {"more"};
+            next = new bool[] {true};
+            responses = new string[]
+            {
+                "\\DYI will \\Ytotally \\DYmake a sequel, I just need more info on Bart and more experience in C#. Ciao!"
+            };
+            initializeScene(settingText, actions, responses, next);
+
+            settingText = "\\DYType \"\\Yquit\\DY\" to quit the game.";
+            actions = new string[] {"this game sucked by the way!!!"};
+            next = new bool[] {false};
+            responses = new string[] {"\\DR\\L9O\\L9k\\L9.\\L9.\\L9."};
+            initializeScene(settingText, actions, responses, next);
+            
+            // Bye!
         }
     }
 }
